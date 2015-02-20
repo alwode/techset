89fe26e0d1d69e22906d5c0106ac15286e02bb39
alex@wodniok.de <alex@wodniok.de>
2015-02-20 23:27:41 +0100

    Integrate with MySQL

--- techset/grails-app/conf/BootStrap.groovy
+++ techset/grails-app/conf/BootStrap.groovy
@@ -2,12 +2,18 @@ class BootStrap {
 
     def init = { servletContext ->
 		
-	def master = new tal.model.TalMaster(talMasterName: "ICT Software", talMasterDescription: "ICT Software" )
-	  .addToTalDomains(new tal.model.TalDomain(talDomainName: "Application Development", talDomainDescription: "Application Development")
-	    .addToTalGroups(new tal.model.TalGroup(talGroupName: "Release and Deployment Management",talGroupDescription: "Release and Deployment Management")
+	def master = new tal.model.TalMaster(talMasterName: "ICT Software",
+										 talMasterDescription: "ICT Software" )
+	  .addToTalDomains(new tal.model.TalDomain(talDomainName: "Application Development",
+		  									   talDomainDescription: "Application Development")
+	    .addToTalGroups(new tal.model.TalGroup(talGroupName: "Release and Deployment Management",
+											   talGroupDescription: "Release and Deployment Management")
 		  .addToTalAreas(new tal.model.TalArea(talAreaName: "Application Deployment for Legacy and Client/Server", 
-			  								  talAreaDescription: "Application Deployment for Legacy and Client/Server"))))
-	  .addToTalDomains(new tal.model.TalDomain(talDomainName: "Application Integration and Middleware", talDomainDescription: "Application Integration and Middleware"))
+			  								   talAreaDescription: "Application Deployment for Legacy and Client/Server"))
+		  .addToTalAreas(new tal.model.TalArea(talAreaName: "Application Deployment for Legacy and Client/Server", 
+			  								   talAreaDescription: "Application Deployment for Legacy and Client/Server"))))
+	  .addToTalDomains(new tal.model.TalDomain(talDomainName: "Application Integration and Middleware",
+		  									   talDomainDescription: "Application Integration and Middleware"))
 	  .save()
     }
     def destroy = {
